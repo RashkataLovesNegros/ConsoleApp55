69472c14b86449e6c98883cc31e424a96b9f8bdf
MEGA <MEGA@gmail.com>
2025-02-28 17:07:20 +0200

    Add project files.

---
+++ ConsoleApp55/Company.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    internal class Company
+    {
+        
+        private List<Employee> employees;
+
+      
+        public Company()
+        {
+            employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        
+        public string FindEmployeeBySalary(int salary)
+        {
+           
+            foreach (Employee emp in employees)
+            {
+                if (emp.Salary == salary)
+                {
+                    return emp.Name;
+                }
+            }
+            
+            return "Не е намерен";
+        }
+
+    }
+}
---
+++ ConsoleApp55/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Company company = new Company();
+
+           
+            company.AddEmployee(new Employee("Иван", 1000));
+            company.AddEmployee(new Employee("Мария", 1500));
+            company.AddEmployee(new Employee("Петър", 1500));
+            company.AddEmployee(new Employee("Георги", 2000));
+
+          
+            string foundName = company.FindEmployeeBySalary(1500);
+            Console.WriteLine(foundName); 
+
+            
+            foundName = company.FindEmployeeBySalary(3000);
+            Console.WriteLine(foundName);
+        }
+    }
+}
